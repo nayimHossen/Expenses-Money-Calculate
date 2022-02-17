dacb75703a7f589d7f3841c1d181cb0b5df0b7cc
Nayim hossen <nayimh2015@gmail.com>
2022-02-17 18:09:17 +0600

    80% complate with alert

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
-function alertBox(message) {
+//show alert massage function
+function alertMassage(message) {
     document.getElementById("custom-alert").style.display = 'block';
     const para = document.createElement("p");
     para.innerHTML = message;
     document.getElementById("error-message").appendChild(para);
 }
 
-document.getElementById('expensesTotal').addEventListener('click', function () {
-    const foodCost = document.getElementById("food-cost").value;
-    const rantCost = document.getElementById("rant-cost").value;
-    const clothesCost = document.getElementById("clothes-cost").value;
+//expenses total calculation area
+function expensesTotal() {
     const income = document.getElementById('income').value;
+    const foodCost = document.getElementById('food-cost').value;
+    const rantCost = document.getElementById('rant-cost').value;
+    const clothesCost = document.getElementById('clothes-cost').value;
     const totalCost = parseFloat(foodCost) + parseFloat(rantCost) + parseFloat(clothesCost);
 
-    if (foodCost >= 0 && rantCost >= 0 && clothesCost >= 0) {
-        const newTotalCost = totalCost + '';
-        document.getElementById("total-expenses").innerText = newTotalCost;
+    //if input value is a negative number
+    if (foodCost >= 0 && rantCost >= 0 && clothesCost >= 0 && income >= 0) {
 
-        if (income > 0 && income > totalCost) {
+        //if total expenses is greater than income
+        if (income > totalCost) {
+            document.getElementById("total-expenses").innerText = totalCost;
             const remainingBalance = income - totalCost;
             document.getElementById('balance').innerText = remainingBalance;
-        }
-        else {
-            alertBox("Expenses are more than your income.")
+        } else {
+            alertMassage("Expenses are more than your income.");
         }
     }
     else {
-        alertBox("The expenses input field deserve a positive number.")
+        alertMassage("Your input field deserve a positive number.");
     }
-});
 
+}
 
-document.getElementById('savingCalculation').addEventListener('click', function () {
+//saving calculation function area
+function savingCalculation() {
     const income = document.getElementById('income').value;
-    const savingPercent = document.getElementById('saveIng-percent').value;
     const balance = document.getElementById('balance').innerText;
+    const savingPercent = document.getElementById('saveIng-percent').value;
+
     const percent = (savingPercent / 100) * income;
+    const savedAmount = document.getElementById('saving-amount').innerText = percent;
 
-    if (balance > percent) {
+    //if saved amount is greater than balance
+    if (balance > savedAmount) {
         document.getElementById('saving-amount').innerText = percent;
-
-        const remainingBalance = balance - percent;
-        document.getElementById('remaining-balance').innerText = remainingBalance;
+        const remainBalance = balance - percent;
+        document.getElementById('remaining-balance').innerText = remainBalance;
+    } else {
+        alertMassage("Your saving money is greater than your balance.");
     }
-    else {
-        alertBox("Your saving money is greater than your balance.")
-    }
-});
-
+}
